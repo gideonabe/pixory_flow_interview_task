0ae547379c4c184b2047572ae81440a386eb93bc
Gideon Abe <gideonabe2020@gmail.com>
2026-05-28 11:23:47 +0100

    'fixed the layout height issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,7 +63,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body
-        className={`${inter.variable} ${robotoMono.variable} font-sans antialiased min-h-screen`}
+        className={`${inter.variable} ${robotoMono.variable} font-sans antialiased overflow-x-hidden`}
       >
         <Navbar />
         <main>{children}</main>
